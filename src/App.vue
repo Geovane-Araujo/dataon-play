e4cd0a9735e2f603672c15cee357f7ed0bd42195
Geovane Araujo <geovane-araujo@outlook.com>
2021-02-03 10:46:04 -0300

    Campos invisiveis

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@
         <b-button to="/vendas" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/vendas.png"/> Vendas</b-button>
         <b-button to="/compras" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/compras.png"/> Compras</b-button>
         <b-button to="/servicos" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/servicos.png"/> Serviços</b-button>
-        <b-button to="/estoque" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/estoque.png"/> Estoque</b-button>
+        <b-button v-show="visible" to="/estoque" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/estoque.png"/> Estoque</b-button>
         <b-button to="/financeiro" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/financeiro.png"/> Financeiro</b-button>
-        <b-button to="/utilitarios" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/utilitarios.png"/> Utilitários</b-button>
-        <b-button style="color:#ffffff;" variant="outline-dark">Minha DataOn</b-button>
+        <b-button v-show="visible"  to="/utilitarios" style="color:#ffffff;" variant="outline-dark"><img class="icons" src="./assets/utilitarios.png"/> Utilitários</b-button>
+        <b-button v-show="visible"  style="color:#ffffff;" variant="outline-dark">Minha DataOn</b-button>
       </b-button-group>
     </div>
     <div id="nav">
@@ -22,6 +22,16 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 body{
