1cc4de160e9bd953bd4905e5a060727d2eec166d
Testes DataOn <testes@dataon.com.br>
2021-02-04 10:19:45 -0300

    index js petshop

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Veterinaria.vue')
   },
   {
+    path: '/petshop',
+    name: 'Petshop',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Petshop.vue')
+  },
+  {
     path: '/vendas',
     name: 'Vendas',
     // route level code-splitting
