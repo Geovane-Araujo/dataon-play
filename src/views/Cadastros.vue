5090d93576d20abf20865a50912eac58a85eb80d
Testes DataOn <testes@dataon.com.br>
2021-03-23 16:30:41 -0300

    linhas cadastro

--- src/views/Cadastros.vue
+++ src/views/Cadastros.vue
@@ -1,24 +1,49 @@
 <template>
   <div class="cadastros">
     <h3 style="text-align:left;margin-bottom:0px;margin-left:30px">Sequência Recomendada</h3>
-    <div class="cont">
-      <vue-horizontal-list :items="cadastros" :options="options">
-        <template v-slot:default="{ item }">
-          <div class="master">
-            <div class="image-container">
-              <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
-            </div>
-            <div class="subtitle">
-              <div>Sequência</div>
-              <div>{{ item.id + "/" + cadastros.length }}</div>
-            </div>
-            <h4 style="text-align:left;">{{ item.title }}</h4>
-            <p style="text-align:left;" >{{ item.context }}</p>
-            <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
-            <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
-          </div>
-        </template>
-      </vue-horizontal-list>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="cont">
+          <vue-horizontal-list :items="cadastros" :options="options">
+            <template v-slot:default="{ item }">
+              <div class="master">
+                <div class="image-container">
+                  <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+                </div>
+                <div class="subtitle">
+                  <div>Sequência</div>
+                  <div>{{ item.id + "/" + cadastros.length }}</div>
+                </div>
+                <h4 style="text-align:left;">{{ item.title }}</h4>
+                <p style="text-align:left;" >{{ item.context }}</p>
+                <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
+                <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
+              </div>
+            </template>
+          </vue-horizontal-list>
+        </div>
+      </div>
+      <div class="col-sm-12" style="margin-top: 35px">
+        <div class="cont">
+          <vue-horizontal-list :items="cadastros1" :options="options">
+            <template v-slot:default="{ item }">
+              <div class="master">
+                <div class="image-container">
+                  <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+                </div>
+                <div class="subtitle">
+                  <div>Sequência</div>
+                  <div>{{ item.id + "/" + cadastros.length }}</div>
+                </div>
+                <h4 style="text-align:left;">{{ item.title }}</h4>
+                <p style="text-align:left;" >{{ item.context }}</p>
+                <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
+                <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
+              </div>
+            </template>
+          </vue-horizontal-list>
+        </div>
+      </div>
     </div>
     <b-modal id="modal-center" size="lg" centered :title="obj.title">
       <iframe allowfullscreen="allowfullscreen" :src="obj.link" type="application/x-shockwave-flash" width="760" height="500"></iframe>
@@ -77,9 +102,11 @@ export default {
           title: 'Cadastro de Contas Bancárias',
           dificudade: 2,
           img: 'https://videos.dataon.com.br/imagensprincipal/CadCB.jpg'
-        },
+        }
+      ],
+      cadastros1: [
         {
-          id: 5,
+          id: 1,
           context: 'Neste vídeo estaremos mostrando a rotina de cadastro de produtos a granel',
           link: 'https://www.youtube.com/embed/FLa9SQQ4jek',
           title: 'Cadastro de Produtos a Granel',
@@ -87,7 +114,7 @@ export default {
           img: 'https://videos.dataon.com.br/imagensprincipal/CadGranel.jpg'
         },
         {
-          id: 6,
+          id: 2,
           context: 'Neste vídeo estaremos mostrando a rotina de cadastro de usuários',
           link: 'https://www.youtube.com/embed/hz3yfpQ6rcg',
           title: 'Cadastro de usuários',
@@ -95,7 +122,7 @@ export default {
           img: 'https://videos.dataon.com.br/imagensprincipal/CadUsuario.jpg'
         },
         {
-          id: 7,
+          id: 3,
           context: 'Neste vídeo estaremos mostrando a rotina de como cadastrar contas PIX',
           link: 'https://www.youtube.com/embed/Tuy5709yYW8',
           title: 'Cadastro de Contas PIXX',
@@ -103,7 +130,7 @@ export default {
           img: 'https://videos.dataon.com.br/imagensprincipal/contapix.jpg'
         },
         {
-          id: 8,
+          id: 4,
           context: 'Neste vídeo estaremos mostrando a rotina de como Copiarmos Permissões de Usuário',
           link: 'https://www.youtube.com/embed/-3hoRzo2NLM',
           title: 'Cópia de Permissões de Usuário',
