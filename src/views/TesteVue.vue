0bc1c0fea7df01ef5b181e60e59f17c54e6feb90
Testes DataOn <testes@dataon.com.br>
2021-04-08 11:56:31 -0300

    vet

--- src/views/TesteVue.vue
+++ src/views/TesteVue.vue
@@ -1,74 +1,76 @@
 <template>
-  <div class="cadastros">
-    <h3 style="text-align:left;margin-bottom:0px;margin-left:30px">Sequência Recomendada</h3>
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="cont">
-          <vue-horizontal-list :items="cadastros" :options="options">
-            <template v-slot:default="{ item }">
-              <div class="master">
-                <div class="image-container">
-                  <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+  <div v-for="item in master" :key="item.cadastro"
+    <div class="cadastros">
+        <h3 style="text-align:left;margin-bottom:0px;margin-left:30px">Sequência Recomendada</h3>
+        <div class="row">
+        <div class="col-sm-12">
+            <div class="cont">
+            <vue-horizontal-list :items="cadastros" :options="options">
+                <template v-slot:default="{ item }">
+                <div class="master">
+                    <div class="image-container">
+                    <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+                    </div>
+                    <div class="subtitle">
+                    <div>Sequência</div>
+                    <div>{{ item.id + "/" + cadastros.length }}</div>
+                    </div>
+                    <h4 style="text-align:left;">{{ item.title }}</h4>
+                    <p style="text-align:left;" >{{ item.context }}</p>
+                    <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
+                    <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
                 </div>
-                <div class="subtitle">
-                  <div>Sequência</div>
-                  <div>{{ item.id + "/" + cadastros.length }}</div>
-                </div>
-                <h4 style="text-align:left;">{{ item.title }}</h4>
-                <p style="text-align:left;" >{{ item.context }}</p>
-                <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
-                <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
-              </div>
-            </template>
-          </vue-horizontal-list>
+                </template>
+            </vue-horizontal-list>
+            </div>
         </div>
-      </div>
-      <div class="col-sm-12" style="margin-top: 35px">
-        <div class="cont">
-          <vue-horizontal-list :items="cadastros1" :options="options">
-            <template v-slot:default="{ item }">
-              <div class="master">
-                <div class="image-container">
-                  <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+        <div class="col-sm-12" style="margin-top: 35px">
+            <div class="cont">
+            <vue-horizontal-list :items="cadastros1" :options="options">
+                <template v-slot:default="{ item }">
+                <div class="master">
+                    <div class="image-container">
+                    <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+                    </div>
+                    <div class="subtitle">
+                    <div>Sequência</div>
+                    <div>{{ item.id + "/" + cadastros.length }}</div>
+                    </div>
+                    <h4 style="text-align:left;">{{ item.title }}</h4>
+                    <p style="text-align:left;" >{{ item.context }}</p>
+                    <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
+                    <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
                 </div>
-                <div class="subtitle">
-                  <div>Sequência</div>
-                  <div>{{ item.id + "/" + cadastros.length }}</div>
-                </div>
-                <h4 style="text-align:left;">{{ item.title }}</h4>
-                <p style="text-align:left;" >{{ item.context }}</p>
-                <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
-                <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
-              </div>
-            </template>
-          </vue-horizontal-list>
+                </template>
+            </vue-horizontal-list>
+            </div>
         </div>
-       </div>
-       <div class="col-sm-12" style="margin-top: 35px">
-          <div class="cont">
-          <vue-horizontal-list :items="cadastros2" :options="options">
-            <template v-slot:default="{ item }">
-              <div class="master">
-                <div class="image-container">
-                  <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+        <div class="col-sm-12" style="margin-top: 35px">
+            <div class="cont">
+            <vue-horizontal-list :items="cadastros2" :options="options">
+                <template v-slot:default="{ item }">
+                <div class="master">
+                    <div class="image-container">
+                    <button v-b-modal.modal-center @click="obj=item"><img class="img" :src="item.img"/></button>
+                    </div>
+                    <div class="subtitle">
+                    <div>Sequência</div>
+                    <div>{{ item.id + "/" + cadastros.length }}</div>
+                    </div>
+                    <h4 style="text-align:left;">{{ item.title }}</h4>
+                    <p style="text-align:left;" >{{ item.context }}</p>
+                    <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
+                    <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
                 </div>
-                <div class="subtitle">
-                  <div>Sequência</div>
-                  <div>{{ item.id + "/" + cadastros.length }}</div>
-                </div>
-                <h4 style="text-align:left;">{{ item.title }}</h4>
-                <p style="text-align:left;" >{{ item.context }}</p>
-                <h6 style="text-align:left;font-size: 10px" >Dificuldade</h6>
-                <b-form-rating class="stars" id="rating-sm" size="sm" color="#ff8800" v-model="item.dificudade" stars="5"></b-form-rating>
-              </div>
-            </template>
-          </vue-horizontal-list>
+                </template>
+            </vue-horizontal-list>
+            </div>
         </div>
-       </div>
+        </div>
+        <b-modal id="modal-center" size="lg" centered :title="obj.title">
+        <iframe allowfullscreen="allowfullscreen" :src="obj.link" type="application/x-shockwave-flash" width="760" height="500"></iframe>
+        </b-modal>
     </div>
-    <b-modal id="modal-center" size="lg" centered :title="obj.title">
-      <iframe allowfullscreen="allowfullscreen" :src="obj.link" type="application/x-shockwave-flash" width="760" height="500"></iframe>
-    </b-modal>
   </div>
 </template>
 
